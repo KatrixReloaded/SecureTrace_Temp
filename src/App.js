c93005582a7286759256c66d49af1cf48900b521
kartikeyasureka <kartikeyasureka@gmail.com>
2024-09-19 02:54:53 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import RecentTransactionsTable from './dashboard/recentTxs/TxTable.jsx';
+import TokenDetails from './addressPortfolioTracker/page/TokenDetails.jsx';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<RecentTransactionsTable />} />
+        {/* <Route path="/" element={<RecentTransactionsTable />} /> */}
+        <Route path="/" element={<TokenDetails />} />
       </Routes>
     </BrowserRouter>
   );
