b298792788a6538186e1c08edb9a39627f9252f1
kartikeyasureka <kartikeyasureka@gmail.com>
2024-09-19 02:27:25 +0530

    Arbitrum portfolio complete

--- src/addressPortfolioTracker/networks/arbitrum.js
+++ src/addressPortfolioTracker/networks/arbitrum.js
@@ -1,6 +1,24 @@
-// Get ERC20-Token Account Balance for TokenContractAddress
-const TOKENBALANCE = 'https://api.arbiscan.io/api?module=account&action=tokenbalance&contractaddress=0xfc5a1a6eb076a2c7ad06ed22c90d7e710e35ad0a&address=0x1aeedd3727a6431b8f070c0afaa81cc74f273882&tag=latest&apikey=YourApiKeyToken';
+const axios = require('axios');
+require('dotenv').config();
+const { Alchemy, Network } = require('alchemy-sdk');
+const { ethers } = require('ethers');
 
-// Get Historical ERC-20 Token Account Balance by ContractAddress & BlockNo PRO VERSION ONLY
-const TOKENBALANCEHISTORY = 'https://api.arbiscan.io/api?module=account&action=tokenbalancehistory&contractaddress=0x831753dd7087cac61ab5644b308642cc1c33dc13&address=0x019ba0325f1988213d448b3472fa1cf8d07618d7&blockno=20169066&apikey=YourApiKeyToken';
+const ADDRESS = '0x400FF1835A4A11C7E77B12240EB227D5EcC75767';
+const settings = {
+    apiKey: process.env.ALCHEMY_APIKEY,
+    network: Network.ARB_MAINNET,
+}
 
+const alchemy = new Alchemy(settings)
+
+async function fetchAddressDetails() {
+    const balances = await alchemy.core.getTokenBalances(ADDRESS);
+    for(const token of balances.tokenBalances) {
+        const metadata = await alchemy.core.getTokenMetadata(token.contractAddress);
+        const readableBalance = ethers.formatUnits(token.tokenBalance, metadata.decimals);
+        console.log(`Token: ${metadata.name}`);
+        console.log(`Balance: ${readableBalance} ${metadata.symbol}`);
+    }
+}
+
+fetchAddressDetails();
