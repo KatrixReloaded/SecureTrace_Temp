b2391d1a32f3a6d9024d89209b22c1858e3e3b21
KatrixReloaded <kartikeyasureka@gmail.com>
2024-09-20 18:21:22 +0530

    App.js now shows APT

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import RecentTransactionsTable from './dashboard/recentTxs/TxTable.jsx';
 import TokenDetails from './addressPortfolioTracker/page/TokenDetails.jsx';
+import TokenTransfers from './txTracingVisualizer/networks/TokenTransfers.jsx';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         {/* <Route path="/" element={<RecentTransactionsTable />} /> */}
-        <Route path="/" element={<TokenDetails />} />
+        {/* <Route path="/" element={<TokenDetails />} /> */}
+        <Route path="/" element={<TokenTransfers />} />
       </Routes>
     </BrowserRouter>
   );
