df81006239d16b6ed887a3bb0bd08787a86d5d4c
KatrixReloaded <kartikeyasureka@gmail.com>
2024-09-21 15:59:21 +0530

    added natspec and merged with APT

--- src/txTracingVisualizer/networks/addressVisualizer.js
+++ src/txTracingVisualizer/networks/addressVisualizer.js
@@ -5,8 +5,12 @@ const { Alchemy, Network } = require('alchemy-sdk');
 const { ethers } = require('ethers');
 const cors = require('cors');
 
+/** --------------------------------------------------------------------- 
+-------------------------  GLOBAL VARIABLES -----------------------------
+---------------------------------------------------------------------- */
+
 const app = express();
-const port = 3002;
+const port = process.env.PORT || 3001;
 
 const settingsArbitrum = {
     apiKey: process.env.ALCHEMY_APIKEY,
@@ -16,6 +20,7 @@ const settingsEthereum = {
     apiKey: process.env.ALCHEMY_APIKEY,
     network: Network.ETH_MAINNET, 
 };
+//@note Linea is not supported by Alchemy SDK functions like getAssetTransfers() and getTokenBalances()
 const settingsLinea = {
     apiKey: process.env.ALCHEMY_APIKEY,
     network: Network.LINEA_MAINNET, 
@@ -28,6 +33,7 @@ const settingsOptimism = {
     apiKey: process.env.ALCHEMY_APIKEY,
     network: Network.OPT_MAINNET, 
 };
+//@note Blast is not supported by Alchemy SDK functions like getAssetTransfers()
 const settingsBlast = {
     apiKey: process.env.ALCHEMY_APIKEY,
     network: Network.BLAST_MAINNET, 
@@ -49,6 +55,117 @@ app.use(cors({
 
 app.use(express.json());
 
+
+/** --------------------------------------------------------------------- 
+-------------------------- COMMON FUNCTIONS -----------------------------
+---------------------------------------------------------------------- */
+
+
+/** @dev checks whether the token is a valid/official token and not some bs*/
+async function fetchTokenList() {
+    try {
+        const response = await axios.get('https://tokens.coingecko.com/uniswap/all.json');
+        const tokens = response.data.tokens;
+        return new Set(tokens.map(token => token.address.toLowerCase())); 
+    } catch (error) {
+        console.error('Error fetching token list:', error);
+        return new Set(); 
+    }
+}
+
+
+/** --------------------------------------------------------------------- 
+------------------------- PORTFOLIO TRACKER -----------------------------
+---------------------------------------------------------------------- */
+
+
+/** @dev fetchAddressDetails fetches the address's ERC-20 token assets
+ * @param settings -> alchemy settiings for different chains
+ * @param address -> the address value for which the tokens are being fetched
+ */
+async function fetchAddressDetails(settings, address) {
+    let validTokenAddresses = await fetchTokenList();
+    const alchemy = new Alchemy(settings);
+    const balances = await alchemy.core.getTokenBalances(address);
+    const tokenDetails = [];
+    for(const token of balances.tokenBalances) {
+        const metadata = await alchemy.core.getTokenMetadata(token.contractAddress);
+        if (!metadata || metadata.decimals === 0 || !metadata.name || !metadata.symbol) {
+            console.log(`Skipping token: ${token.contractAddress}, missing metadata`);
+            continue;
+        }
+        if (!validTokenAddresses.has(token.contractAddress.toLowerCase())) {
+            console.log(`Skipping token: ${token.contractAddress}, not in valid token list`);
+            continue;
+        }
+        const readableBalance = ethers.formatUnits(token.tokenBalance, metadata.decimals);
+        console.log(`Token: ${metadata.name} (${metadata.symbol}), Balance: ${readableBalance} ${token.tokenBalance}`);
+        if(parseFloat(readableBalance) > 0) {
+            tokenDetails.push({tokenBalance: readableBalance, tokenName: metadata.name, tokenSymbol: metadata.symbol});
+            console.log({tokenBalance: readableBalance, tokenName: metadata.name, tokenSymbol: metadata.symbol});
+        }
+    }
+    return(tokenDetails);
+}
+
+/** @dev just in case we need to set a timeout 
+ * @param target function to set a timeout for it
+*/
+async function fetchWithTimeout(fetchFunction, timeout = 120000) {
+    const abortController = new AbortController();
+    const id = setTimeout(() => abortController.abort(), timeout);
+    try {
+        const result = await fetchFunction();
+        clearTimeout(id);
+        return result;
+    } catch (error) {
+        throw new Error('Request timed out');
+    }
+}
+
+/** @dev address value is passed here and tokens across multiple chains are checked */
+/** @param req -> req.body == the address passed*/
+app.post('/fetch-address-details', async (req, res) => {
+    const { address } = req.body;
+
+    if (!address) {
+        return res.status(400).json({ error: 'Address is required' });
+    }
+    let tokens = [];
+    try {
+        console.log("Fetching arb assets");
+        tokens.push(await fetchAddressDetails(settingsArbitrum, address));
+        console.log("Fetching avax assets");
+        tokens.push(await fetchAddressDetails(settingsAvalanche, address));
+        console.log("Fetching blast assets");
+        tokens.push(await fetchAddressDetails(settingsBlast, address));
+        console.log("Fetching eth assets");
+        tokens.push(await fetchAddressDetails(settingsEthereum, address));
+        console.log("Fetching linea assets");
+        // tokens.push(await fetchAddressDetails(settingsLinea, address));
+        console.log("Fetching opt assets");
+        tokens.push(await fetchAddressDetails(settingsOptimism, address));
+        console.log("Fetching pol assets");
+        tokens.push(await fetchAddressDetails(settingsPolygon, address));
+        console.log("Fetching zk assets");
+        tokens.push(await fetchAddressDetails(settingsZksync, address));
+        res.json({ tokens });
+    } catch (error) {
+        console.error('Error fetching address details:', error);
+        res.status(500).json({ error: 'Failed to fetch address details' });
+    }
+});
+
+
+/** --------------------------------------------------------------------- 
+---------------------------- ADDRESS TTV --------------------------------
+---------------------------------------------------------------------- */
+
+
+/** @dev function to fetch all transfers made out from the given address
+ * @param settings -> alchemy settings for different chains
+ * @param address -> the address value from which the transfers have been made
+ */
 async function tokenTransfersFrom(settings, address) {
     const alchemy = new Alchemy(settings);
 
@@ -67,6 +184,10 @@ async function tokenTransfersFrom(settings, address) {
     });
 }
 
+/** @dev function to fetch transfers made to the given address
+ * @param settings -> alchemy settings for different chains
+ * @param address -> the address value to which the transfers have been made
+ */
 async function tokenTransfersTo(settings, address) {
     const alchemy = new Alchemy(settings);
     
@@ -85,6 +206,9 @@ async function tokenTransfersTo(settings, address) {
     });
 }
 
+/** @dev address value is passed here to fetch all to and from transfer of tokens from that address
+ * @param req -> req.params.address == the address passed
+ */
 app.get('/token-transfers/:address', async (req, res) => {
   const address = req.params.address;
   const allFromTransfers = [];
@@ -112,7 +236,6 @@ app.get('/token-transfers/:address', async (req, res) => {
         allToTransfers.push(...avaxToTransfers.toTransfers.transfers);
 
         /* ---------- getAssetTransfers() DOES NOT SUPPORT THESE CHAINS ---------- */
-        
         // console.log('Fetching transfers for Blast');
         // const blastFromTransfers = await tokenTransfersFrom(settingsBlast, address);
         // const blastToTransfers = await tokenTransfersTo(settingsBlast, address);
