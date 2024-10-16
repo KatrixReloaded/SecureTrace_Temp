8b624899c6b9941e22f4b7c0605e45433aa7e683
KatrixReloaded <kartikeyasureka@gmail.com>
2024-10-17 00:17:07 +0530

    and a comma

--- src/back-end/TokenDBMySQL.js
+++ src/back-end/TokenDBMySQL.js
@@ -1,12 +1,13 @@
 const mysql = require('mysql2/promise'); // Use promise-based API for async/await
 const axios = require('axios');
+require('dotenv').config();
 
 // MySQL connection settings
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'SQLkatrix1004@',
-    database: 'tokenDB',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10, // Adjust this based on your needs
     queueLimit: 0,
