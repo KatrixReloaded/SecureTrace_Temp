0ab01c92af245b338628391681dd285e001dd0ec
KatrixReloaded <kartikeyasureka@gmail.com>
2024-10-25 14:58:20 +0530

    Trying to fake a conflict

--- src/back-end/SecureTrace.js
+++ src/back-end/SecureTrace.js
@@ -724,6 +724,34 @@ app.get('/recent-txs', async (req, res) => {
     }
 });
 
+
+
+/** -----------------------------------------------------------------------------
+ * --------------------------- TRENDING TOKENS PAGE -----------------------------
+ * --------------------------------------------------------------------------- */
+
+app.get('/top-tokens', async (req, res) => {
+    try {
+        tokenNameToId = await fetchTokenData();
+        const evmTokenIds = new Set(tokenNameToId.map(token => token.id));
+        const response = await axios.get('https://api.coingecko.com/api/v3/coins/markets', {
+            params: {
+                vs_currency: 'usd',
+                order: 'market_cap_desc',
+                per_page: 10,
+                page: 1
+            }
+        });
+
+        const topEvmTokens = response.data.filter(token => evmTokenIds.has(token.id));
+
+        res.json(topEvmTokens);
+    } catch (error) {
+        console.error("Failed to fetch top EVM tokens:", error);
+        res.status(500).json({ error: 'Unable to retrieve top EVM token data.' });
+    }
+});
+
 /// @note add balance history, figure out how you can fetch the data once you click on a token value
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
